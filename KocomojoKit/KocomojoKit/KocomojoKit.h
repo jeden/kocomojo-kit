a2e7360277dc7403b2b777f7399bc1c2ba5bed22
Antonio Bello <jeden@elapsus.com>
2015-01-25 22:23:47 +0100

    Created workspace and projects

---
+++ KocomojoKit/KocomojoKit/KocomojoKit.h
@@ -0,0 +1,19 @@
+//
+//  KocomojoKit.h
+//  KocomojoKit
+//
+//  Created by Antonio Bello on 1/25/15.
+//  Copyright (c) 2015 Elapsus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KocomojoKit.
+FOUNDATION_EXPORT double KocomojoKitVersionNumber;
+
+//! Project version string for KocomojoKit.
+FOUNDATION_EXPORT const unsigned char KocomojoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KocomojoKit/PublicHeader.h>
+
+
